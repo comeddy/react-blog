b3229cc60a7399fdd79eb6550c6525e67ff0b4c2
oscar <comeddy@users.noreply.github.com>
2021-07-10 12:59:39 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [title, changeTitle] = useState(['남자 코트 추천','강남 고기 맛집', '파이썬독학']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div> Dev Blog</div>
+      </div>
+      <div className="list">
+        <h3>{ title[0] } <span></span>1</h3>
+        <p>7월 10일 발행 </p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ title[1] }</h3>
+        <p>7월 10일 발행 </p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ title[2] }</h3>
+        <p>7월 10일 발행 </p>
+        <hr/>
+      </div>
     </div>
   );
 }
