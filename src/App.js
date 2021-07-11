9fd51f21f0a4c2052d412d30b74be16bbfc878cc
oscar <comeddy@users.noreply.github.com>
2021-07-11 13:38:02 +0900

    add2

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import './App.css';
 
 function App() {
   let [title, changeTitle] = useState(['남자 코트 추천','강남 고기 맛집', '파이썬독학']);
+  let [likeCount, changeCount] = useState(0);
+
+  // function changeTitleName() {
+  //   var newArray = [...title];
+  //   newArray[0] = '여자 코트 추천';
+  //   changeTitle(newArray);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div> Dev Blog</div>
       </div>
+      {/* <button onClick={ changeTitleName } > 버튼 </button> */}
       <div className="list">
-        <h3>{ title[0] } <span></span>1</h3>
+        <h3>{ title[0] } <span onClick={ ()=>{changeCount(likeCount +1)}}>😊</span> {likeCount}</h3>
         <p>7월 10일 발행 </p>
         <hr/>
       </div>
       <div className="list">
-        <h3>{ title[1] }</h3>
+        <h3>{ title[1] } <span>😊</span></h3>
         <p>7월 10일 발행 </p>
         <hr/>
       </div>
@@ -26,8 +34,20 @@ function App() {
         <p>7월 10일 발행 </p>
         <hr/>
       </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="Modal">
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
 }
 
 export default App;
